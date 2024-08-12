2c99b821c501f8d29fc6a331dd52214e87927b62
klepa <nklepatskii@marfa-tech.com>
2024-08-12 18:58:26 +0300

    add test with mock

--- src/app/command/create_short_url.rs
+++ src/app/command/create_short_url.rs
@@ -4,6 +4,7 @@ use uuid::Uuid;
 use crate::app::error::AppError;
 use crate::domain::url::url::Url;
 
+#[mockall::automock]
 #[async_trait]
 pub trait CreateShortUrlRepository {
     async fn create(&self, url: Url) -> Result<String, AppError>;
@@ -35,5 +36,21 @@ where
         };
         
         self.repo.create(new_url).await
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn get_short_url() {
+        let mut mock_repo = MockCreateShortUrlRepository::new();
+        mock_repo.expect_create().returning(|_| Ok("short.ly/abc123".to_string())).times(1);
+        let command = CreateShortUrlCommand::new(mock_repo);
+        let result = command.execute("https://www.google.com".to_string()).await;
+        
+        assert!(result.is_ok());
+        assert_eq!(result.unwrap(), "short.ly/abc123".to_string());
     }
 }
